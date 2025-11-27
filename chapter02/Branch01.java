50d65f22faea356a373c6c116fc7ba19fe9ae443
kimjiwoo-hi <rlawldn1015@gmail.com>
2025-11-27 10:56:21 +0900

    version4_g

---
+++ chapter02/Branch01.java
@@ -0,0 +1,9 @@
+package chapter02;
+
+public class Branch01 {
+    
+    public Branch01(){
+        System.out.println("구글 브렌치에서 작업");
+        System.out.println("다음 브랜치에서 작업");
+    }
+}
--- chapter02/Branch02.java
+++ chapter02/Branch02.java
@@ -3,6 +3,7 @@ package chapter02;
 public class Branch02 {
     
     public Branch02(){
-        System.out.println("네이버 브랜치에서 작업");
+        System.out.println("충돌 테스트");
+        System.out.println("다음 브랜치에서 작업");
     }
 }
